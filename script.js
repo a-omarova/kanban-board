3d1cc43d3ee10f038fee30876414d19d75f77ed6
Amina Omarova <mamikami@yandex.ru>
2019-04-24 23:50:49 +0300

    added scrol, fixed add column, changed add column

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ var CARDS_LIST = 'cards-list';
 var ACTIONS_OPEN_DIALOG_MOD = 'column__actions_open-dialog';
 
 var addColumnActions = `<div class="column__actions column__actions_column">
-                            <input class="dialog dialog_column" placeholder="Введите название колонки">
+                            <div class="column__input">
+                                <input class="dialog dialog_column" placeholder="Введите название колонки">
+                            </div>
                             <div class="buttons">
                                 <svg class="buttons__icon js-close-dialog" width="15" height="15" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" clip-rule="evenodd" d="M14.141 6.641H8.36V.86A.88.88 0 0 0 7.5 0a.88.88 0 0 0-.859.859V6.64H.86A.88.88 0 0 0 0 7.5c0 .458.4.859.859.859H6.64v5.782c0 .458.401.859.859.859a.88.88 0 0 0 .859-.859V8.36h5.782A.88.88 0 0 0 15 7.5a.88.88 0 0 0-.859-.859z" fill="#6B808C"/>
@@ -24,7 +26,7 @@ var addColumnActions = `<div class="column__actions column__actions_column">
                         </div>`;
 
 var container = document.querySelector('.container');
-var columns = Array.from(document.querySelectorAll('.column'));
+var columnsCounter = document.querySelectorAll('.column').length;
 var createNewColumn = function () {
     var column = document.createElement('section');
 
@@ -51,23 +53,23 @@ container.onclick = function(event) {
 
     if (isOpenCardDialogBtn) {
         target.closest('.' + ACTIONS_CLASS).classList.add(ACTIONS_OPEN_DIALOG_MOD);
-        if (target.closest('.column__actions_column') && columnLength < 4) {
+        if (target.closest('.column__actions_column') && columnsCounter < 4) {
             container.appendChild(createNewColumn());
+            columnsCounter++;
         }
     }
 
     if (isCloseCardDialogBtn) {
-        console.log('columnLength', columnLength);
         target.closest('.' + ACTIONS_CLASS).classList.remove(ACTIONS_OPEN_DIALOG_MOD);
-        if (target.closest('.column__actions_column') && columnLength >= 2) {
+        if (target.closest('.column__actions_column') && columnsCounter > 2 ) {
             container.removeChild(target.closest('.column'));
-            console.log('target.closest(\'.column\').previousSibling', target.closest('.column').previousElementSibling);
+            columnsCounter--;
         }
     }
 
     if (isAddCardBtn) {
         var cardDialog = target.closest('.' + ACTIONS_CLASS).querySelector('.dialog_card');
-
+        
         if (cardDialog.value.length !== 0) {
             target.closest('.column').querySelector('.' + CARDS_LIST).appendChild(createNewCard(cardDialog.value))
             cardDialog.value = '';
@@ -75,4 +77,3 @@ container.onclick = function(event) {
     }
 
 };
-
